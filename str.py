40a817e8e9944134fafe5f247eba776275d0ffc8
Sonu637815 <shersiya.sonu31@gmail.com>
2025-06-12 10:02:35 +0530

    new

---
+++ str.py
@@ -0,0 +1,46 @@
+##string is immutable -> cannot be changed
+## denoted by - '', "" , """ """
+
+##print(name)
+##print(type{name})
+
+##print(name.upper{})
+##print(name.lower{})
+##print(name.title{})
+##print(name.capitalize{})
+##print(name.casefold{})
+##print(name.swapcase{})
+##print(name.strip{})
+##print(name.lstrip{})
+##print(name.rstrip{})
+##print(name.replace{})
+##print(name.split("f"))
+##print(name.partition(" "))
+##print(name.startswitch("u"))
+##print(name.endswitch("d"))
+##print(name.isalpha())
+##print(name.encode())
+##print(name.zfill())
+##print(name.center(30,"*"))
+##print(name.ljust())
+##print(name.rjust())
+
+
+##indexing and slicing
+##indexing -- accessing single character from the string
+##slicing -- accessing multiple character from the string
+
+##name = "UpfLairspvt"
+##print(name[0])
+
+##print(name[0:7])
+
+
+##formatted string //f-string
+##age = 10
+## text ="my agee is" + age
+##text = "my age is {age}"
+##print(text)
+
+text = "my name is \"sonu\""
+print(text)
